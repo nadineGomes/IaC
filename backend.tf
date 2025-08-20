1c078fef3beb318f69dc052a584764600a013f57
nadinegomes <nadine.gomes144@gmail.com>
2025-08-19 22:27:54 -0300

    vpc nginx

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
-    backend "s3" {
-        bucket         = "tf-backend-iac"
-        key            = "projeto-iaC/terraform.tfstate"
-        region         = "us-east-1"
-        dynamodb_table = "terraform-state-lock"
-        encrypt        = true
-      
-    }
+  backend "s3" {
+    bucket         = "tf-backend-iac"
+    key            = "projeto-iaC/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+
+  }
 }
--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -9,10 +9,68 @@ data "aws_ami" "latest_ubuntu" {
 
 }
 
+resource "aws_iam_role" "ssm_role" {
+  name = "ec2_ssm_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "ec2_ssm_instance_profile"
+  role = aws_iam_role.ssm_role.name
+}
+
+resource "aws_security_group" "web_server_sg" {
+  name        = "web_server_sg"
+  description = "Security group for web server"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    # Permite o tráfego SSH de entrada (porta 22) - RECOMENDADO APENAS PARA SEU IP!
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [" 0.0.0.0/0"] # Substitua por seu IP específico para maior segurança
+  }
+
+  # Permite todo o tráfego de saída (essencial para o SSM e para baixar o Docker)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.latest_ubuntu.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.latest_ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = "minha-chave-aws"
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name
+
   user_data = <<-EOF
               #!/bin/bash
               
@@ -41,19 +99,8 @@ resource "aws_instance" "web_server" {
     Environment = var.env
     provisioner = "web_server_terraform"
     Repo        = var.repo
-        
+
   }
 }
 
-resource "aws_security_group" "web_server_sg" {
-  name        = "web_server_sg"
-  description = "Security group for web server"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
